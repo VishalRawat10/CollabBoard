f6459eac29da901e9963d78a784038f5ce295682
Vishal Rawat <omniverserawat2005@gmail.com>
2025-07-06 12:38:51 +0530

    Removed updated task bug

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -12,7 +12,7 @@ module.exports.editTask = async (req, res) => {
         return res.status(400).json({ message: "Incomplete task information." });
     }
     try {
-        const task = await Task.findByIdAndUpdate(taskId, { title, description, assignedTo, status, priority, dueDate });
+        const task = await Task.findByIdAndUpdate(taskId, { title, description, assignedTo, status, priority, dueDate }, { new: true });
         return res.status(200).json({ message: "task updated successfully", task });
 
     } catch (err) {
